30e7a7c9cf6ad8020b4bdb7342067bbd1d1ad90d
Dhiraj Kuttichirayil <dhirajk1@gmail.com>
2022-12-26 19:14:04 -0500

    optimizing day 14

--- Day14/solution_day14.py
+++ Day14/solution_day14.py
@@ -1,11 +1,16 @@
 """Solution for Darow 14"""
 import numpy as np
 
+SAND = 2
+WALL = 1
+
 
 def solve(file_name: str) -> tuple[int, int]:
     """Solves both parts"""
+
     grid = np.zeros((200, 1000))
-    limit = 0
+    bottom = 0
+
     with open(file_name, "r") as file:
         for line in file:
             rock_ranges = line.strip().split("->")
@@ -16,28 +21,32 @@ def solve(file_name: str) -> tuple[int, int]:
                     start_col, start_row, end_col, end_row = points
                     start_col, end_col = sorted([start_col, end_col])
                     start_row, end_row = sorted([start_row, end_row])
-                    grid[start_row : end_row + 1, start_col : end_col + 1] = 1
-                    limit = max(limit, end_row)
+                    grid[start_row : end_row + 1, start_col : end_col + 1] = WALL
+                    bottom = max(bottom, end_row)
                 elif len(window) == 1:
                     col, row = points
-                    grid[row, col] = 1
-    grid[limit + 2] = 1
-    part1 = None
-    col, row = 500, 0
-    while grid[row, col] != 2:
-        while grid[row + 1, col] == 0 or np.any(grid[row + 1, col - 1 : col + 2] == 0):
-            row += 1
-            if grid[row, col] == 0:
-                continue
-            elif grid[row, col - 1] == 0:
+                    grid[row, col] = WALL
+
+    grid[bottom + 2] = WALL
+    part1 = part2 = 0
+    while grid[0, 500] != SAND:
+        row, col = 0, 500
+        while grid[row, col] != SAND:
+            if not part1 and row == bottom:
+                part1 = part2
+            if not grid[row + 1, col]:
+                row += 1
+            elif not grid[row + 1, col - 1]:
+                row += 1
                 col -= 1
-            else:
+            elif not grid[row + 1, col + 1]:
+                row += 1
                 col += 1
-            if not part1 and row == limit:
-                part1 = (grid == 2).sum()
-        grid[row, col] = 2
-        col, row = 500, 0
-    return part1, (grid == 2).sum()
+            else:
+                grid[row, col] = SAND
+                part2 += 1
+
+    return part1, part2
 
 
 if __name__ == "__main__":
